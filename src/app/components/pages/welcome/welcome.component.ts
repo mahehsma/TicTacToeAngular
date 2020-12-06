8308d7c89783f87c9981d52f12041521297ab958
mahehsma <56866320+mahehsma@users.noreply.github.com>
2020-12-06 17:26:56 +0100

    up

--- src/app/components/pages/welcome/welcome.component.ts
+++ src/app/components/pages/welcome/welcome.component.ts
@@ -19,14 +19,14 @@ export class WelcomeComponent implements OnInit {
   changeP2Visibility() {
     // const p2Input = document.getElementById('p2Input');
     // p2Input?.classList.add('visibility: hidden');
-    this.p2Input="";
+   // this.p2Input="";
  
 
-
+/* 
     if (document.getElementById('pvaiToggle')?.getAttribute('checked')) {
       document.getElementById('p2Input').style.visibility = 'visible';
     } else {
       document.getElementById('p2Input')?.style.visibility = 'hidden';
-    }
+    }*/
   }
 }
