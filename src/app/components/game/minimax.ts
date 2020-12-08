a010b4772849a56490c8b7d472d4cd925fb0338d
mahehsma <56866320+mahehsma@users.noreply.github.com>
2020-12-08 10:08:49 +0100

    minimax update

--- src/app/components/game/minimax.ts
+++ src/app/components/game/minimax.ts
@@ -1,27 +1,24 @@
 import { Board } from './board';
-import { Field } from './field';
 import { State } from './state';
 
 export class Minimax{
-    myFigure: String;
-    enemyFigure: String;
-    constructor(myFigure: String, enemyFigure: String){
+    myFigure: string;
+    enemyFigure: string;
+    constructor(myFigure: string, enemyFigure: string){
         this.myFigure = enemyFigure;
         this.enemyFigure = enemyFigure;
     }
 
     bestmove(board: Board): number{
         let clonedBoard: Board = board.clone();//has to be cloned
-        //board clone
-
         let value= -1;
         let bestValue= -1;
         let move = 0;
         for(let i=0; i<9; i++){
             if(clonedBoard.fields[i].isEmpty()){
-                clonedBoard.fields[i].state(this.myFigure);
+                clonedBoard.fields[i].state=this.myFigure;
                 value = this.minimax(clonedBoard, false);
-                clonedBoard.fields[i].state("");
+                clonedBoard.fields[i].state="";
                 if(value>bestValue){
                     bestValue = value;
                     move = i;
@@ -33,10 +30,39 @@ export class Minimax{
 
     minimax(clonedBoard: Board,isMaximizing: boolean): number{
         let state = new State();
+        let bestValue;
+        let value;
         if(state.isDraw(clonedBoard)){
             return 0;
         }
-        return -1;
+        if(isMaximizing){
+            if(state.hasWon(clonedBoard, this.enemyFigure)){
+                return -Infinity;
+            }
+            bestValue = -Infinity;
+            for(let i = 0; i<9; i++){
+                if(clonedBoard.fields[i].isEmpty()){
+                    clonedBoard.fields[i].state=this.myFigure;
+                    value = this.minimax(clonedBoard, false);
+                    clonedBoard.fields[i].state="";
+                    bestValue = Math.max(value, bestValue);
+                }
+            }
+        } else{
+            if(state.hasWon(clonedBoard, this.myFigure)){
+                return Infinity;
+            }
+            bestValue = Infinity;
+            for(let i = 0; i<9; i++){
+                if(clonedBoard.fields[i].isEmpty()){
+                    clonedBoard.fields[i].state=this.enemyFigure;
+                    value = this.minimax(clonedBoard, true);
+                    clonedBoard.fields[i].state="";
+                    bestValue = Math.min(value, bestValue);
+                }
+            }
+        }
+        return bestValue;
     }
 
 }
